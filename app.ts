31f615d7eb521175c90d46b344b9acc6655250ab
Matt Noakes <mattnoakes@me.com>
2021-04-18 23:59:08 +0100

    Bring up to date and multipart related

--- app.ts
+++ app.ts
@@ -1,5 +1,4 @@
-//import * as path from "https://deno.land/std@0.92.0/path/mod.ts";
-import { opine, json } from "https://deno.land/x/opine@1.2.0/mod.ts";
+import { opine, json } from "https://deno.land/x/opine@1.3.2/mod.ts";
 
 import indexRouter from './routes/index.ts';
 
--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,7 @@
-import { Router } from 'https://deno.land/x/opine@1.2.0/mod.ts';
+import { Router, Request, Response, NextFunction } from 'https://deno.land/x/opine@1.3.2/mod.ts';
+
+//import { multiParser, Form } from 'https://deno.land/x/multiparser@v2.1.0/mod.ts';
+import { parse } from "https://raw.githubusercontent.com/mayankchoubey/deno-body-parser/main/mod.ts"
 
 const router = Router();
 
@@ -17,7 +20,7 @@ const maxLength = 10000;
 
 
 /** A function which takes an incoming POST request and prints it, raw, to the screen and to a file */
-function echoMultipartPost(req: any, res: any, _next: any) {
+async function echoMultipartPost(req: Request, res: Response, _next: NextFunction) {
 
     /** A function for writing out to the console (optionally snipped) and to a new file */
     const echo = (() => { 
@@ -47,20 +50,22 @@ function echoMultipartPost(req: any, res: any, _next: any) {
     let contentType = '';
 
     // Print the request headers and obtain Content-Type
-    for (const header in req.headers) {
-        echo(header + ": " + req.headers[header]);
+    for (const header in req.headers.keys()) {
+        echo(header + ": " + req.headers.get(header));
         if (header.toLowerCase() === 'content-type') {
-            contentType = req.headers[header].toLowerCase();
+            contentType = req.headers.get(header)!.toLowerCase();
         }
     }
 
     // Attempt to parse the MIME boundary, if there is one specified
     const boundary = (() => {
         const BOUNDARY_PARSER = /;\s*boundary=(.*?)\s*(;|$)/i;
-        const parsed = BOUNDARY_PARSER.exec(req.headers['content-type']);
+        const parsed = BOUNDARY_PARSER.exec(req.headers.get('content-type') || '');
         const boundary = (parsed && parsed.length) ? (parsed[1] || parsed[2]) : "";
         return boundary.replace(/\"/g, "");
     })();
+
+    console.log({ boundary });
 
     // No boundary indentified => single part content
     if (!boundary.length) {
@@ -99,7 +104,40 @@ function echoMultipartPost(req: any, res: any, _next: any) {
     //const mr = new mime.MultipartReader('', boundary);
     console.log(`${BREAK}^^ MULTIPART POST LOGGED @ ${new Date()} ^^${BREAK}`);
 
+    // deno-body-parser will parse raw text
+    const body = await parse(req, { unknownAsText: true });
 
+    if (body) {
+        const { txt }: { txt: string, json: Record<string, unknown>} = body;
+        //console.log({ txt, json });
+        const contentType = req.headers.get('content-type');
+        //console.log({ contentType });
+
+        if (contentType?.toLowerCase().trim().startsWith('multipart/alternative')) {
+            // TODO multipart/related etc.
+
+            // Maanually split the text by the boundary
+            const parts = txt.trim()
+                .split(`--${boundary}`)
+                .map(p => p.trim().split('\n\n'));
+            console.log(parts);
+
+            // Map the headers to an object and the body to a string property
+            const multipartMessage = parts
+                .filter(p => p.length === 2)
+                .map(([headers, body]) => ({
+                    headers: Object.assign(
+                        {},
+                        ...headers.split('\n')
+                            .map(h => h.split(':'))
+                            .map(([key, value]) => ({ [key.trim()]: value.trim() }))
+                    ),
+                    body
+                }));
+            
+            console.log(multipartMessage);
+        }
+    }
     /*
     var form = new multiparty.Form();
     form.on('part', function (part) {
@@ -157,8 +195,8 @@ function echoMultipartPost(req: any, res: any, _next: any) {
 
 router.post('/echo', echoMultipartPost);
 
-router.post('/test', (req: any, res: any, _next: any) => {
-    console.log(req.body);
+router.post('/test', (_req, res, _next) => {
+    //console.log(req.body);
     res.sendStatus(200);
 });
 
